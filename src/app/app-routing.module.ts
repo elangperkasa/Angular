56483a48b0478536975c3a6c145f615f39afe7cd
Erwin Kurnia <erwin.kurnia@gmail.com>
2019-02-18 20:55:46 +0700

    Update app-routing.module.ts
    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MeComponent } from  './me/me.component';
+import { ContactComponent } from  './ui/contact/contact.component';
+import { JourneyComponent } from  './ui/journey/journey.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+{
+path:  'contact',
+component:  ContactComponent
+}
+,
+{
+path:  'journey',
+component:  JourneyComponent
+}
+,
+{
+path:  '',
+component:  MeComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
